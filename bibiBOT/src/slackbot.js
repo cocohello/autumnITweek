c428ea272ee926adccb266eab00e43b820b65f25
cocohello <cocohello010@naver.com>
2018-08-15 14:42:47 +0900

    slack bot-default

---
+++ bibiBOT/src/slackbot.js
@@ -0,0 +1,42 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+//import modules for setting slack connection and create an instance of middleware.
+const Botkit = require('botkit');
+const dialogflowMiddleware = require('botkit-middleware-dialogflow')({
+  keyFilename: './src/autumn-it-week-2018-69982756a8f8.json',  // service account private key file from GCP
+});
+
+
+//initiates controller obeject of Slack
+const slackController = Botkit.slackbot();//reference https://botkit.ai/docs/readme-slack.html#create-a-controller
+
+//get bot object which is a worker to connect to slack..
+const slackBot = slackController.spawn({
+  token: process.env.SLACK_TOKEN// Slack API Token connected to dialogflow's bot
+});
+
+//use receive middleware to process the message each time Slack emits a message to Botkit
+slackController.middleware.receive
+	.use(dialogflowMiddleware.receive);
+
+// tell bot to start connection
+slackBot.startRTM();
+
+//catch message from slack
+slackController
+	.hears(
+		'.*', 
+		['direct_message', dialogflowMiddleware.hears], 
+		( bot, message ) => {
+			slackController.trigger('input.unknown', [bot, message]);
+			/*replyText = message.fulfillment.text;  // message object has new fields added by Dialogflow
+			bot.reply(message, replyText);*/
+		}
+	);
+
+slackController
+	.on('input.unknown', (bot, message) => {
+		bot.reply(message, 'まだまだですねえ');
+	});
